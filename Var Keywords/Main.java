cf07bfd3c56cd5c47f22fa50230946e5518d4e62
Birendra Bahadur Budha <birendrabahadurbudhaeng99@gmail.com>
2024-03-07 15:20:04 +0530

    Adding Var Keywords Files

---
+++ Var Keywords/Main.java
@@ -0,0 +1,21 @@
+class Students{
+    // var age=22;
+    public void printInfo(){
+        var age=23;
+        System.out.println("Age: "+age);
+    }
+    {
+        var name="Ram";
+
+    }
+    
+}
+
+public class Main{
+    public static void main(String[] args) {
+        var age=20;
+        System.out.println("age");
+        Students first=new Students();
+        first.printInfo();
+    }
+}
